026ec554cf3652170b471a543307649814a25f6c
bulbulalamgir <bulbulalamgir@gmail.com>
2020-07-15 18:02:57 +0530

    Second commit

---
+++ routes/bills.js
@@ -0,0 +1,7 @@
+// Base URL - /api/v1/bills/
+
+// / - GET - get all bills
+// / - POST - create bill
+// /:id - GET - get single bill
+// /:id - PUT - update bill
+// /:id - delete - delete bill
---
+++ routes/payments.js
@@ -0,0 +1,7 @@
+// Base URL - /api/v1/payments/
+
+// / - GET - get all payments
+// / - POST - create payment
+// /:id - GET - get single payment
+// /:id - PUT - update payment
+// /:id - delete - delete payment
